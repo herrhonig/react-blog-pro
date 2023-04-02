021d709ab0608cd9cb558eead2d7327ffd342a4f
Nick B <nikita.bukof@gmail.com>
2023-04-02 15:29:52 +0300

    MAIN: Added jest + config + tests on classNames module

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,42 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+    test('With 1st param only', () => {
+        expect(classNames('someClass')).toBe('someClass');
+    });
+
+    test('With additional class', () => {
+        const expected = 'someClass class1 class2';
+        expect(classNames('someClass', {}, ['class1', 'class2'])).toBe(expected);
+    });
+
+    test('With mods', () => {
+        const expected = 'someClass class1 class2 hovered scrollable';
+        expect(classNames(
+            'someClass',
+            { hovered: true, scrollable: true },
+            ['class1', 'class2'],
+        ))
+            .toBe(expected);
+    });
+
+    test('With mods false', () => {
+        const expected = 'someClass class1 class2 hovered';
+        expect(classNames(
+            'someClass',
+            { hovered: true, scrollable: false },
+            ['class1', 'class2'],
+        ))
+            .toBe(expected);
+    });
+
+    test('With mods undef', () => {
+        const expected = 'someClass class1 class2 hovered';
+        expect(classNames(
+            'someClass',
+            { hovered: true, scrollable: undefined },
+            ['class1', 'class2'],
+        ))
+            .toBe(expected);
+    });
+});
--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,11 +1,15 @@
 type Mods = Record<string, boolean | string>;
 
-export function classNames(cls: string, mods?: Mods, additional?: string[]): string {
+export function classNames(
+    cls: string,
+    mods: Mods = {},
+    additional: string[] = [],
+): string {
     return [
         cls,
         ...additional.filter(Boolean),
         ...Object.entries(mods)
-            .filter(([_, value]) => !!value) // Фильтруем моды, где value === true;
+            .filter(([className, value]) => !!value) // Фильтруем моды, где value === true;
             .map(([className, value]) => className), // Возвращает массив classnames, где value === true после фильтра
     ]
         .join(' ');
