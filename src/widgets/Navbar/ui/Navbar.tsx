cdcc73eafc47a1d2f371efdb2445cf91f4f1acb7
Nick B <nikita.bukof@gmail.com>
2023-05-29 00:26:48 +0300

    Fixed ts problem - storybook is next

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames';
 
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
@@ -9,11 +10,15 @@ interface Props {
     className?: string; // этот класс стилей принимается снаружи чтобы поправить стили вроде отступов или другие доп момменты.
 }
 
-export const Navbar: React.FC<Props> = ({ className }) => (
-    <div className={classNames(cls.Navbar, {}, [className])}>
-        <div className={cls.links}>
-            <AppLink theme={AppLinkTheme.SECONDARY} to="/" className={cls.mainLink}>ГЛАВНАЯ</AppLink>
-            <AppLink theme={AppLinkTheme.SECONDARY} to="/about">О САЙТЕ</AppLink>
+export const Navbar: React.FC<Props> = ({ className }) => {
+    const { t } = useTranslation(); // <- Передаем название namespace "about". По дефолту - "translation"
+
+    return (
+        <div className={classNames(cls.Navbar, {}, [className])}>
+            <div className={cls.links}>
+                <AppLink theme={AppLinkTheme.SECONDARY} to="/" className={cls.mainLink}>{t('Главная')}</AppLink>
+                <AppLink theme={AppLinkTheme.SECONDARY} to="/about">{t('О сайте')}</AppLink>
+            </div>
         </div>
-    </div>
-);
+    );
+};
