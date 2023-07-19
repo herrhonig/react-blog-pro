631d4291466ae1d0d5351491d2c73444f657b661
Nick B <nikita.bukof@gmail.com>
2023-07-19 08:38:21 +0300

    MAIN: Added custom input + login form + modal window lazy load

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,20 +1,12 @@
-/* eslint-disable i18next/no-literal-string */
-import React, { useState } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Input } from 'shared/ui/Input/ui/Input';
 
 const MainPage = () => {
     const { t } = useTranslation('main');
-    const [value, setValue] = useState<string>('');
-
-    const onChange = (value: string) => {
-        setValue(value);
-    };
 
     return (
         <div>
             {t('Главная страница')}
-            <Input value={value} onChange={onChange} placeholder="Insert text..." />
         </div>
     );
 };
