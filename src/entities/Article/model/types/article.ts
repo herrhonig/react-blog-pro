0784d30355730bf07e82b057a5fd757cd26827a4
bukov.nikita <bukov.nikita@wb.ru>
2024-04-29 20:05:44 +0300

    feat: Added article entity + types

---
+++ src/entities/Article/model/types/article.ts
@@ -0,0 +1,44 @@
+export type ArticleType = 'IT' | 'SCIENCE' | 'ECONOMICS';
+
+export type ArticleBlockType = 'CODE' | 'TEXT' | 'IMAGE' | 'WARNING';
+
+export interface ArticleBlockBase {
+    id: string;
+    type: ArticleBlockType;
+}
+/**
+ * Несколько типов блоков, их которых строится статья.
+ * Например, текстовый блок или блок с кодом;
+ * ||
+ * \/
+ */
+
+export interface ArticleCodeBlock extends ArticleBlockBase {
+    type: 'CODE';
+    code: string;
+}
+
+export interface ArticleImageBlock extends ArticleBlockBase {
+    type: 'IMAGE';
+    src: string;
+    title: string;
+}
+
+export interface ArticleTextBlock extends ArticleBlockBase {
+    type: 'TEXT';
+    paragraphs: string[];
+    title?: string;
+}
+
+export type ArticleBlock = ArticleTextBlock | ArticleImageBlock | ArticleCodeBlock;
+
+export interface Article {
+    id: string;
+    title: string;
+    subtitle: string;
+    img: string;
+    views: number;
+    createdAt: string;
+    type: ArticleType[];
+    blocks: ArticleBlock[];
+}
